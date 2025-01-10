c3380163f8cac55e65a4fa25e880ef4195b8e4af
Marco Antonio - markitos DevSecOps Culture <markitos.es.info@gmail.com>
2025-01-10 18:41:21 +0100

    - test para tokener pero el payload hay que hacerlo un struct

--- testsuite/infrastructure/libs/tokener.jwt_test.go
+++ testsuite/infrastructure/libs/tokener.jwt_test.go
@@ -1,33 +1,23 @@
 package libs_test
 
-import (
-	"log"
-	infraLibs "markitos-golang-service-access/infrastructure/libs"
-	"markitos-golang-service-access/internal/domain"
-	"strings"
-	"testing"
-	"time"
+// func xTestCanCreateAValidJWTToken(t *testing.T) {
+// 	var masterTokenValue string = domain.UUIDv4()
+// 	var duration time.Duration = time.Minute
+// 	var issuedAt time.Time = time.Now()
+// 	var expireAt time.Time = issuedAt.Add(duration)
+// 	log.Println("expireAt", expireAt)
 
-	"github.com/stretchr/testify/require"
-)
+// 	tokener := infraLibs.NewTokenerJWT("any-secret-key-this-is-for-developer-use")
+// 	token, err := tokener.Create(masterTokenValue, duration)
+// 	require.NoError(t, err)
+// 	require.NotEmpty(t, token)
+// 	parts := strings.Split(token, ".")
+// 	require.Equal(t, 3, len(parts))
 
-func xTestCanCreateAJWTTokenWithAValidSecretKeyLength(t *testing.T) {
-	var payload string = domain.UUIDv4()
-	var duration time.Duration = time.Minute
-	var issuedAt time.Time = time.Now()
-	var expireAt time.Time = issuedAt.Add(duration)
-	log.Println("expireAt", expireAt)
-
-	tokener := infraLibs.NewTokenerJWT("any-secret-key-this-is-for-developer-use")
-	token, err := tokener.Create(payload, duration)
-	require.NoError(t, err)
-	require.NotEmpty(t, token)
-	parts := strings.Split(token, ".")
-	require.Equal(t, 3, len(parts))
-
-	validatedPayload, err := tokener.Validate(token)
-	require.NoError(t, err)
-	require.NotEmpty(t, validatedPayload)
-	require.Equal(t, payload, validatedPayload)
-
-}
+// 	validatedPayload, err := tokener.Validate(token)
+// 	require.NoError(t, err)
+// 	require.NotEmpty(t, validatedPayload)
+// 	require.Equal(t, masterTokenValue, validatedPayload.id)
+// 	require.WithinDuration(t, issuedAt, validatedPayload.issuedAt, time.Microsecond)
+// 	require.WithinDuration(t, expireAt, validatedPayload.expireAt, time.Microsecond)
+// }
